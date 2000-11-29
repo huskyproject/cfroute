b0cc68b53ccaf4e98855b692bb363cf9423586bf
tobi <>
2000-11-29 21:55:13 +0000

    fixed makefile

--- win/makeit.bat
+++ win/makeit.bat
@@ -1,6 +1,8 @@
-bcc32 %1 -c -ocfroute.obj -I..\src -DSQUISHCFS -I..\..\smapi -WC -DWIN -D__NT__ -DWINNT ..\src\cfroute.cpp
-bcc32 %1 -c -ocfroute.obj -I..\src -DSQUISHCFS -I..\..\smapi -WC -DWIN -D__NT__ -DWINNT ..\src\buffer.cpp
-bcc32 %1 -c -ocfroute.obj -I..\src -DSQUISHCFS -I..\..\smapi -WC -DWIN -D__NT__ -DWINNT ..\src\structs.cpp
-bcc32 %1 -c -odirute.obj -I..\src -DSQUISHCFS -I..\..\smapi -WC -DWIN -D__NT__ -DWINNT ..\src\dirute.c
-bcc32 %1 -c -odirute.obj -I..\src -DSQUISHCFS -I..\..\smapi -WC -DWIN -D__NT__ -DWINNT ..\src\fecfg146.c
-bcc32 -L..\..\smapi -WC -ecfroute.exe cfroute.obj dirute.obj structs.obj buffer.obj fecfg146.obj smapibcw.lib 
+bcc32 %1 -c -ocfroute.obj -I..\src -DSQUISHCFS -I..\..\smapi -I..\.. -WC -DWIN -D__NT__ -DWINNT ..\src\cfroute.cpp
+bcc32 %1 -c -ocfroute.obj -I..\src -DSQUISHCFS -I..\..\smapi -I..\.. -WC -DWIN -D__NT__ -DWINNT ..\src\buffer.cpp
+bcc32 %1 -c -ocfroute.obj -I..\src -DSQUISHCFS -I..\..\smapi -I..\.. -WC -DWIN -D__NT__ -DWINNT ..\src\structs.cpp
+bcc32 %1 -c -odirute.obj -I..\src -DSQUISHCFS -I..\..\smapi -I..\.. -WC -DWIN -D__NT__ -DWINNT ..\src\dirute.c
+bcc32 %1 -c -odirute.obj -I..\src -DSQUISHCFS -I..\..\smapi -I..\.. -WC -DWIN -D__NT__ -DWINNT ..\src\fecfg146.c
+bcc32 -L..\..\smapi -WC -ecfroute.exe cfroute.obj dirute.obj structs.obj buffer.obj fecfg146.obj smapibcw.lib
+bcc32 %1 -c -fc2cfr.obj -I..\src -DSQUISHCFS -I..\..\smapi -I..\..\fidoconf -I..\.. -WC -DWIN -D__NT__ -DWINNT ..\src\fc2cfr.obj
+bcc32 -L..\..\smapi -L..\..\fidoconf -WC -efc2cfr.exe fc2cfr.obj fconfbcw.lib smapibcw.lib
